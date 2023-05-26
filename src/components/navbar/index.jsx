6e529588f3c3e51104b2a159169855d4b5e1d3ff
Maritana Alves <maritana.cz@gmail.com>
2023-05-26 17:45:59 -0300

    first commit

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+// import { Button } from '@mui/material'
+
+export default function NavBar() {
+  return (
+    <div className="topo">
+      <span>Qual a palavra?</span>
+      <button>Configurações</button>
+    </div>
+  )
+}
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import Board from '../../components/board'
+import NavBar from '../../components/navbar'
+
+function Home() {
+  return (
+    <>
+      <NavBar />
+      <Board />
+    </>
+  )
+}
+
+export default Home

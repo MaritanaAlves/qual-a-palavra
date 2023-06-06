95131fdce431727b68520ebc2e7c395dd63863e2
Maritana Alves <maritana.cz@gmail.com>
2023-06-05 23:59:55 -0300

    creating keyboard

--- src/components/board/index.jsx
+++ src/components/board/index.jsx
@@ -1,5 +1,20 @@
 import './style.css'
 
 export default function Board() {
-  return <div></div>
+  return (
+    <div>
+      <button id="kb-q">Q</button>
+      <button id="kb-q">W</button>
+      <button id="kb-q">E</button>
+      <button id="kb-q">R</button>
+      <button id="kb-q">T</button>
+      <button id="kb-q">Y</button>
+      <button id="kb-q">U</button>
+      <button id="kb-q">I</button>
+      <button id="kb-q">O</button>
+      <button id="kb-q">P</button>
+      <button id="kb-q">Q</button>
+      <button id="kb-q">Q</button>
+    </div>
+  )
 }
